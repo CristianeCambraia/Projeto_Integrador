f43fe5a8121e518fe564ec0318248f449d992e6b
Leonardo <leoalexj50@gmail.com>
2025-09-23 16:25:49 -0300

    ATUALIZADO

--- app/urls.py
+++ app/urls.py
@@ -1,50 +1,36 @@
 from django.urls import path
 from . import views
-
+ 
 urlpatterns = [
     # Página inicial
     path('', views.pagina_home, name='home'),
-
+ 
     # ----- FORNECEDORES -----
     path('fornecedores/', views.abrir_fornecedor, name='fornecedores'),
     path('fornecedores/salvar/', views.salvar_fornecedor, name='salvar_fornecedor'),
     path('fornecedores/lista/', views.lista_fornecedores, name='lista_fornecedores'),
-
+ 
     # ----- PRODUTOS -----
     path('produtos/', views.cadastrar, name='produtos'),  # abrir form + salvar produto
     path('produtos/lista/', views.lista_produtos, name='lista_produtos'),
     path('produtos/cadastrar/', views.cadastrar, name='cadastrar_produto'),
-
+ 
     # ----- CLIENTES -----
     path('clientes/', views.abrir_cliente, name='clientes'),
     path('clientes/salvar/', views.salvar_cliente, name='salvar_cliente'),
     path('clientes/lista/', views.lista_cliente, name='lista_cliente'),
-
+ 
     # ----- USUÁRIOS -----
     path('usuarios/cadastrar/', views.cadastrar_usuario, name='cadastrar_usuario'),
-
+ 
     # ---- ORÇAMENTOS -----
     path('emitir_orcamento/', views.emitir_orcamento, name='emitir_orcamento'),
     path('salvar_orcamento/', views.salvar_orcamento, name='salvar_orcamento'),
     path('orcamentos_emitidos/', views.orcamentos_emitidos, name='orcamentos_emitidos'),
     path('novo_orcamento/', views.novo_orcamento, name='novo_orcamento'),
     path('voltar/', views.voltar, name='voltar'),
-<<<<<<< HEAD
-<<<<<<< HEAD
-
+ 
     # Sobre nós
     path('sobre_nos/', views.sobre_nos, name='sobre_nos'),
-=======
-    path('sobre_nos/',views.sobre_nos,name='sobre_nois')
-
->>>>>>> 8b3d8cb (Arrumando header e centralizando placeholder)
-=======
-    path('sobre_nos/',views.sobre_nos,name='sobre_nois')
-
-=======
-
-    # Sobre nós
-    path('sobre_nos/', views.sobre_nos, name='sobre_nos'),
->>>>>>> 07028afda136ae530191b24b9d748108fee4b877
->>>>>>> eefbede (WIP: alterações locais antes do rebase)
 ]
+ 
