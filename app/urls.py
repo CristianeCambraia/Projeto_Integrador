a9c607a294ffec16c234e5dca2e2f495fa384f1c
Cris <cristiane_cambraia@hotmail.com>
2025-09-30 16:57:14 -0300

    tela cadastro_usuario arrumada

--- app/urls.py
+++ app/urls.py
@@ -4,25 +4,25 @@ from . import views
 urlpatterns = [
     # Página inicial
     path('', views.pagina_home, name='home'),
- 
+
     # ----- FORNECEDORES -----
     path('fornecedores/', views.abrir_fornecedor, name='fornecedores'),
     path('fornecedores/salvar/', views.salvar_fornecedor, name='salvar_fornecedor'),
     path('fornecedores/lista/', views.lista_fornecedores, name='lista_fornecedores'),
- 
+
     # ----- PRODUTOS -----
     path('produtos/', views.cadastrar, name='produtos'),  # abrir form + salvar produto
     path('produtos/lista/', views.lista_produtos, name='lista_produtos'),
     path('produtos/cadastrar/', views.cadastrar, name='cadastrar_produto'),
- 
+
     # ----- CLIENTES -----
     path('clientes/', views.abrir_cliente, name='clientes'),
     path('clientes/salvar/', views.salvar_cliente, name='salvar_cliente'),
     path('clientes/lista/', views.lista_cliente, name='lista_cliente'),
- 
+
     # ----- USUÁRIOS -----
     path('usuarios/cadastrar/', views.cadastrar_usuario, name='cadastrar_usuario'),
- 
+
     # ---- ORÇAMENTOS -----
     path('emitir_orcamento/', views.emitir_orcamento, name='emitir_orcamento'),
     path('salvar_orcamento/', views.salvar_orcamento, name='salvar_orcamento'),
@@ -30,15 +30,12 @@ urlpatterns = [
     path('novo_orcamento/', views.novo_orcamento, name='novo_orcamento'),
     path('voltar/', views.voltar, name='voltar'),
 
+    # ----- SOBRE E SUPORTE -----
     path('sobre_nos/', views.sobre_nos, name='sobre_nos'),
-    path("suporte/novo/", views.criar_suporte, name="criar_suporte"),
-
-
-    path('sobre_nos/',views.sobre_nos,name='sobre_nois'),
+    path('suporte/novo/', views.criar_suporte, name='criar_suporte'),
 
     # ----- RELATÓRIOS -----
     path('relatorio-estoque/', views.relatorio_estoque, name='relatorio_estoque'),
     path('relatorio-entrada/', views.relatorio_entrada, name='relatorio_entrada'),
     path('relatorio-saida/', views.relatorio_saida, name='relatorio_saida'),
 ]
- 
