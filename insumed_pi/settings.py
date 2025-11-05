9b5f4c8dff1bf847a5c7cd9dc31142717e2f5f44
Cris <cristiane_cambraia@hotmail.com>
2025-11-05 09:23:10 -0300

    Remove: Botao colar codigo - volta ao comportamento original

--- insumed_pi/settings.py
+++ insumed_pi/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-6k=&l#zwby=j2ta(jl&ta
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0,172.20.10.2').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
