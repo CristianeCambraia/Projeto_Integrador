4edd55f87457e3a1bf9ab48cda867ba0677af833
Cris <cristiane_cambraia@hotmail.com>
2025-12-04 14:30:40 -0300

    Adiciona responsividade mobile para mensagem de boas-vindas

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,14 +1,24 @@
 def usuario_logado(request):
     try:
+        from .models import Usuario
         context = {
-            'usuario_logado': 'usuario_logado' in request.session,
             'admin_logado': 'admin_logado' in request.session,
             'notificacoes_count': 0
         }
+        
+        if 'usuario_logado' in request.session:
+            try:
+                usuario = Usuario.objects.get(id=request.session['usuario_logado'])
+                context['usuario_logado'] = usuario
+            except Usuario.DoesNotExist:
+                context['usuario_logado'] = None
+        else:
+            context['usuario_logado'] = None
+            
         return context
     except Exception:
         return {
-            'usuario_logado': False,
+            'usuario_logado': None,
             'admin_logado': False,
             'notificacoes_count': 0
         }
